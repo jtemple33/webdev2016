22a1c85ee367a2d541d0ea1a03b30c0c4b61c714
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-14 22:42:36 -0400

    Declare create developer API

--- app/models/developer/developer.model.server.js
+++ app/models/developer/developer.model.server.js
@@ -3,4 +3,13 @@ var mongoose = require("mongoose");
 module.exports = function (db) {
     var DeveloperSchema = require("./developer.schema.server.js")();
     var Developer = mongoose.model("Developer", DeveloperSchema);
+
+    var api = {
+        createDeveloper: createDeveloper
+    };
+    return api;
+
+    function createDeveloper (developer) {
+
+    }
 };
