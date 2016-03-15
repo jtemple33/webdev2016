91ed377bfc00b0612a2138227762dc066e893cec
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-14 23:49:04 -0400

    New Developer Controller

--- public/views/developer/developer.controller.js
+++ public/views/developer/developer.controller.js
@@ -1,9 +1,19 @@
 (function () {
     angular
         .module ("WebAppMakerApp")
-        .controller ("DeveloperListController", developerListController);
+        .controller ("DeveloperListController", developerListController)
+        .controller ("NewDeveloperController", newDeveloperController);
 
     function developerListController () {
         var vm = this;
+    }
+
+    function newDeveloperController () {
+        var vm = this;
+        vm.createDeveloper = createDeveloper;
+
+        function createDeveloper (developer) {
+            console.log (developer);
+        }
     }
 })();
