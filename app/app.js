14fafcdaa98f2080f6ae2e258d5ddcdfde638181
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-16 00:19:52 -0400

    Pass application model to service

--- app/app.js
+++ app/app.js
@@ -2,5 +2,6 @@ module.exports = function (app, db) {
     var developerModel = require ("./models/developer/developer.model.server.js")(db);
     var developerService = require("./services/developer.service.server.js")(app, developerModel);
 
-    var applicationService = require("./services/application.service.server.js")(app);
+    var applicationModel = require("./models/application/application.model.server.js")();
+    var applicationService = require("./services/application.service.server.js")(app, applicationModel);
 };
