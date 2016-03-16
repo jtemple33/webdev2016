349a9de7fdb1c3f51846dfd343fec5bfde91ea26
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-16 00:22:20 -0400

    Create application in model

--- app/models/application/application.model.server.js
+++ app/models/application/application.model.server.js
@@ -2,4 +2,13 @@ var mongoose = require("mongoose");
 module.exports = function () {
     var ApplicationSchema = require("./application.schema.server.js")();
     var Application = mongoose.model("Application", ApplicationSchema);
+
+    var api = {
+        createApplication: createApplication
+    };
+    return api;
+
+    function createApplication (application) {
+
+    }
 };
--- app/services/application.service.server.js
+++ app/services/application.service.server.js
@@ -1,8 +1,10 @@
-module.exports = function (app, developerModel) {
+module.exports = function (app, applicationModel) {
     app.post ("/api/developer/:username/application", createApplication);
 
     function createApplication (req, res) {
         var username = req.params.username;
         var application = req.body;
+        applicationModel
+            .createApplication (application)
     }
 }
