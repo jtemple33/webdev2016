a7489a34fec0a7fb9231da24fac28bddccc08608
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-16 00:07:06 -0400

    Retrieve username and application from request

--- app/services/application.service.server.js
+++ app/services/application.service.server.js
@@ -2,6 +2,7 @@ module.exports = function (app, developerModel) {
     app.post ("/api/developer/:username/application", createApplication);
 
     function createApplication (req, res) {
-
+        var username = req.params.username;
+        var application = req.body;
     }
 }
