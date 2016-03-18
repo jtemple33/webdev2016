374c961529fadf5a6321e06d334f6040208b2952
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-17 21:48:10 -0400

    Application controller requests applications from service

--- public/views/application/application.controller.js
+++ public/views/application/application.controller.js
@@ -4,13 +4,22 @@
         .controller ("ApplicationListController", applicationListController)
         .controller ("NewApplicationController", newApplicationController);
 
-    function applicationListController ($routeParams) {
+    function applicationListController (
+        $routeParams, ApplicationService) {
+
         var vm = this;
         vm.username = $routeParams.username;
+
+        function init () {
+            ApplicationService
+                .findApplicationsForUsername (vm.username)
+        }
+        init ();
     }
 
     function newApplicationController (
         $routeParams, ApplicationService, $location) {
+
         var vm = this;
         vm.username = $routeParams.username;
         vm.createApplication = createApplication;
