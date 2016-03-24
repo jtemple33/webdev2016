8a92f1b9ce0efe123d7f26fb98327eec233a044d
jtemple103@gmail.com <jtemple103@gmail.com>
2016-03-24 16:49:28 -0400

    to client forms service

--- public/assignment/client/services/forms.service.client.js
+++ public/assignment/client/services/forms.service.client.js
@@ -3,17 +3,18 @@
         .module("FormBuilderApp")
         .factory("FormService", FormService);
 
-    function FormService() {
-        var forms = [];
+    function FormService($http) {
+        /*var forms = [];
 
         forms = [ {"_id": "000", "title": "Contacts", "userId": 123},
             {"_id": "010", "title": "ToDo",     "userId": 123},
             {"_id": "020", "title": "CDs",      "userId": 234}];
 
+            */
+
         var api = {
             createFormForUser: createFormForUser,
             findFormById: findFormById,
-            findFormIndexById: findFormIndexById,
             findAllFormsForUser: findAllFormsForUser,
             deleteFormById: deleteFormById,
             updateFormById: updateFormById
@@ -21,6 +22,28 @@
 
         return api;
 
+
+        function createFormForUser(userId, form) {
+            return $http.post("/api/assignment/user/" + userId + "/form", form)
+        }
+
+        function findAllFormsForUser(userId) {
+            return $http.get("/api/assignment/user/" + userId + "/form");
+        }
+
+        function deleteFormById(formId) {
+            return $http.delete("/api/assignment/form/" + formId);
+        }
+
+        function updateFormById(formId, newForm) {
+            return $http.put("/api/assignment/form/" + formId, newForm);
+        }
+
+        function findFormById(formId) {
+            return $http.get("/api/assignment/form/" + formId)
+        }
+
+        /*
         function createFormForUser(userId, form, callback) {
             form._id = (new Date()).getTime();
             form.userId = userId;
@@ -76,7 +99,7 @@
             var form = findFormIndexById(formId);
             forms.splice(form,1,newForm);
             callback(newForm);
-        }
+        }*/
 
     }
 
