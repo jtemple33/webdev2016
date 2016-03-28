dc6463c2dffdaa04703fa0233d36408e63535772
jtemple103@gmail.com <jtemple103@gmail.com>
2016-03-28 19:20:40 -0400

    reorg to index to client folder

--- public/assignment/client/config.js
+++ public/assignment/client/config.js
@@ -6,30 +6,30 @@
     function Configuration($routeProvider) {
         $routeProvider
             .when("/home", {
-                templateUrl: "client/views/home/home.view.html"
+                templateUrl: "views/home/home.view.html"
             })
             .when("/admin", {
-                templateUrl: "client/views/admin/admin.view.html",
+                templateUrl: "views/admin/admin.view.html",
                 controller: "AdminController"
             })
             .when("/forms", {
-                templateUrl: "client/views/forms/forms.view.html",
+                templateUrl: "views/forms/forms.view.html",
                 controller: "FormController"
             })
             .when("/forms/:formId/fields", {
-                templateUrl: "client/views/forms/fields.view.html",
+                templateUrl: "views/forms/fields.view.html",
                 controller: "FieldController"
             })
             .when("/login", {
-                templateUrl: "client/views/users/login.view.html",
+                templateUrl: "views/users/login.view.html",
                 controller: "LoginController"
             })
             .when("/profile", {
-                templateUrl: "client/views/users/profile.view.html",
+                templateUrl: "views/users/profile.view.html",
                 controller: "ProfileController"
             })
             .when("/register", {
-                templateUrl: "client/views/users/register.view.html",
+                templateUrl: "views/users/register.view.html",
                 controller: "RegisterController"
             })
             .otherwise({
